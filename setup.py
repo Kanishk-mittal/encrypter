4a31a725e12c8f1b623a356517015680bcdf01d0
Kanishk mittal <kanishkmittal2005@gmail.com>
2024-02-08 17:27:39 +0530

    ready for upload

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-setup(name="encoder",
+setup(name="encrypter",
 version="1.0.1",
 description="This is an encoder package",
 long_description="This is python package helps us encode a given text using some basic encryption algorithms",
